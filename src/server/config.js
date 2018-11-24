8522b68a337a860db03802f7e424457e77a47954
Ben Shepherd <ben.shepherd@gmx.com>
2018-11-24 12:19:36 +0000

    First commit

---
+++ src/server/config.js
@@ -0,0 +1,10 @@
+let config = {
+    database: {
+        host: '127.0.0.1',
+        username: 'username',
+        password: 'password',
+        database: 'database'
+    }
+}
+
+export default config
---
+++ src/server/routes/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import { view } from '../libs/helper/helpers'
+
+const router = express.Router()
+
+/**
+ * Index, list of available functions
+ */
+router.get('/', async(req, res) => {
+	try {
+		res.sendFile(view('index.html'))
+	}
+	catch(e) {
+		res.status(e.status || 500).json(e)
+	}
+})
